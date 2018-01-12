6c5ed933616b93f219128dfb3a0a87ae24508e29
yanyixin <ymfighting1@gmail.com>
2018-01-13 01:21:24 +0800

    feat(main): 数字计算

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-s1 = 72
-s2 = 85
-r = (float(s2) - float(s1)) / float(s1) * 100
+print('来看看你的成绩及高了多少')
+name = input('请输入你的名字')
+s1 = float(input('请输入去年的成绩'))
+s2 = float(input('请输入今年的成绩'))
+s3 = float((s2-s1)/s1)
 # r = (s2-s1)/s1
-print('恭喜你,今年的成绩提高了%.02f%%,再接再厉！'%(r))
+print('恭喜你，%s, 成绩提高了%.1f%%' %(name, s3))
