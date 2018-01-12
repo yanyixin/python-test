8370b46d3b14b421d6a722a8edd7f1b2d079e515
yanyixin <ymfighting1@gmail.com>
2018-01-12 23:54:26 +0800

    feat(main): 数字计算

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# name = input('please enter your name:')
-# print(name)
-age = 20
-if age >= 18:
-    print('your age is', age)
-    print('adult')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+s1 = 72
+s2 = 85
+r = (float(s2) - float(s1)) / float(s1) * 100
+# r = (s2-s1)/s1
+print('恭喜你,今年的成绩提高了%.02f%%,再接再厉！'%(r))
