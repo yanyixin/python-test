83624cf7058b1fb1fedfb78d8f52b238f9a6efe8
yanyixin <ymfighting1@gmail.com>
2018-01-16 23:28:50 +0800

    feat(): update

--- main.py
+++ main.py
@@ -35,20 +35,21 @@
 # else:
 # 	print('00前')
 
-height = input('请输入你的身高：')
-weight = input('请输入你的体重：')
+height = input('请输入你的身高(m)：')
+weight = input('请输入你的体重(kg)：')
 BMI = weight / (height * height)
 print('您的 BMI 指数为：%.2f'%BMI)
-if BMI > 32:
-	print('严重肥胖')
-elif BMI >= 28 and BMI < 32:
-	print('肥胖')
-elif BMI >= 25 and BMI < 28:
-	print('过重')
-elif BMI >= 18.5 and BMI < 25:
+if BMI < 18.5:
+	print('过轻')
+elif BMI < 25:
 	print('正常')
+elif BMI < 28:
+	print('过重')
+elif BMI < 32:
+	print('肥胖')
 else :
-	print('过轻')	
+	print('严重肥胖')	
+	
 
 
 
