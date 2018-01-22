a56d835909f178a2d5ff70daf27e30a245d47ec1
yanyixin <ymfighting1@gmail.com>
2018-01-22 20:30:54 +0800

    feat(): 递归函数

--- main.py
+++ main.py
@@ -123,37 +123,42 @@
 # 	print('name:', name, 'age:', age, 'love:', love)
 # person('ym', 18, **other)
 
-def product(*nums):
-	if len(nums)  == 0:
-		raise TypeError
-	else:
-		sum = 1
-		for n in nums:
-			sum = sum * n
-		return int(sum)
-# print(product(2,2,3))
+# def product(*nums):
+# 	if len(nums)  == 0:
+# 		raise TypeError
+# 	else:
+# 		sum = 1
+# 		for n in nums:
+# 			sum = sum * n
+# 		return int(sum)
+# # print(product(2,2,3))
 
-# 测试
-print('product(5) =', product(5))
-print('product(5, 6) =', product(5, 6))
-print('product(5, 6, 7) =', product(5, 6, 7))
-print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
-if product(5) != 5:
-    print('5测试失败!')
-elif product(5, 6) != 30:
-    print('5,6测试失败!')
-elif product(5, 6, 7) != 210:
-    print('5,6,7测试失败!')
-elif product(5, 6, 7, 9) != 1890:
-    print('5,6,7,9测试失败!')
-else:
-    try:
-        product()
-        print('测试失败!')
-    except TypeError:
-        print('测试成功!')
+# # 测试
+# print('product(5) =', product(5))
+# print('product(5, 6) =', product(5, 6))
+# print('product(5, 6, 7) =', product(5, 6, 7))
+# print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+# if product(5) != 5:
+#     print('5测试失败!')
+# elif product(5, 6) != 30:
+#     print('5,6测试失败!')
+# elif product(5, 6, 7) != 210:
+#     print('5,6,7测试失败!')
+# elif product(5, 6, 7, 9) != 1890:
+#     print('5,6,7,9测试失败!')
+# else:
+#     try:
+#         product()
+#         print('测试失败!')
+#     except TypeError:
+#         print('测试成功!')
 
 
-
+#  递归
+def fact(n):
+	if n == 1:
+		return 1
+	return n * fact(n-1)
+print(fact(5))
 
 
